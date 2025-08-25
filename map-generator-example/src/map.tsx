6a37fc66e1f190c6ca274f4ff6cd98a2e24dbd78
parkerstovall <parker.stovall@gmail.com>
2025-08-25 09:12:30 -0500

    ok just commenting out the react to do this the rIgHt WaY

--- map-generator-example/src/map.tsx
+++ map-generator-example/src/map.tsx
@@ -1,148 +1,148 @@
-import { useEffect, useState } from 'react'
-import { generateMap } from '../../map-generator/src/map-generator'
-import type { MapGeneratorOptions } from '../../map-generator/src/options'
-import type { Block, BlockMap } from '../../map-generator/src/types'
+// import { useEffect, useState } from 'react'
+// import { generateMap } from '../../map-generator/src/map-generator'
+// import type { MapGeneratorOptions } from '../../map-generator/src/options'
+// import type { Block, BlockMap } from '../../map-generator/src/types'
 
-export default function Map({
-  mapOptions,
-}: {
-  mapOptions: MapGeneratorOptions
-}) {
-  const [map, setMap] = useState<BlockMap>(() => generateMap(mapOptions))
+// export default function Map({
+//   mapOptions,
+// }: {
+//   mapOptions: MapGeneratorOptions
+// }) {
+//   const [map, setMap] = useState<BlockMap>(() => generateMap(mapOptions))
 
-  useEffect(() => {
-    setMap(generateMap(mapOptions))
-  }, [mapOptions])
+//   useEffect(() => {
+//     setMap(generateMap(mapOptions))
+//   }, [mapOptions])
 
-  const getBlockStyle = (block: Block) => {
-    const style: React.CSSProperties = {
-      gridColumn: block.position.x + 1,
-      gridRow: block.position.y + 1,
-    }
+//   const getBlockStyle = (block: Block) => {
+//     const style: React.CSSProperties = {
+//       gridColumn: block.position.x + 1,
+//       gridRow: block.position.y + 1,
+//     }
 
-    if (block.type === 'wall') {
-      if (map[block.position.y][block.position.x + 1]?.type !== 'wall') {
-        if (
-          block.position.y > 0 &&
-          map[block.position.y - 1][block.position.x]?.type !== 'wall'
-        ) {
-          style.borderTopRightRadius = '10px'
-        }
-        if (
-          block.position.y < map.length - 1 &&
-          map[block.position.y + 1][block.position.x]?.type !== 'wall'
-        ) {
-          style.borderBottomRightRadius = '10px'
-        }
-      } else {
-        style.borderRight = 'none'
-      }
+//     if (block.type === 'wall') {
+//       if (map[block.position.y][block.position.x + 1]?.type !== 'wall') {
+//         if (
+//           block.position.y > 0 &&
+//           map[block.position.y - 1][block.position.x]?.type !== 'wall'
+//         ) {
+//           style.borderTopRightRadius = '10px'
+//         }
+//         if (
+//           block.position.y < map.length - 1 &&
+//           map[block.position.y + 1][block.position.x]?.type !== 'wall'
+//         ) {
+//           style.borderBottomRightRadius = '10px'
+//         }
+//       } else {
+//         style.borderRight = 'none'
+//       }
 
-      if (map[block.position.y][block.position.x - 1]?.type !== 'wall') {
-        if (
-          block.position.y > 0 &&
-          map[block.position.y - 1][block.position.x]?.type !== 'wall'
-        ) {
-          style.borderTopLeftRadius = '10px'
-        }
-        if (
-          block.position.y < map.length - 1 &&
-          map[block.position.y + 1][block.position.x]?.type !== 'wall'
-        ) {
-          style.borderBottomLeftRadius = '10px'
-        }
-      } else {
-        style.borderLeft = 'none'
-      }
+//       if (map[block.position.y][block.position.x - 1]?.type !== 'wall') {
+//         if (
+//           block.position.y > 0 &&
+//           map[block.position.y - 1][block.position.x]?.type !== 'wall'
+//         ) {
+//           style.borderTopLeftRadius = '10px'
+//         }
+//         if (
+//           block.position.y < map.length - 1 &&
+//           map[block.position.y + 1][block.position.x]?.type !== 'wall'
+//         ) {
+//           style.borderBottomLeftRadius = '10px'
+//         }
+//       } else {
+//         style.borderLeft = 'none'
+//       }
 
-      if (map[block.position.y - 1]?.[block.position.x]?.type === 'wall') {
-        style.borderTop = 'none'
-      }
+//       if (map[block.position.y - 1]?.[block.position.x]?.type === 'wall') {
+//         style.borderTop = 'none'
+//       }
 
-      if (map[block.position.y + 1]?.[block.position.x]?.type === 'wall') {
-        style.borderBottom = 'none'
-      }
+//       if (map[block.position.y + 1]?.[block.position.x]?.type === 'wall') {
+//         style.borderBottom = 'none'
+//       }
 
-      if (
-        block.position.x === 0 &&
-        map[block.position.y + 1]?.[block.position.x]?.type === 'teleporter'
-      ) {
-        style.borderBottomLeftRadius = 'none'
-      }
+//       if (
+//         block.position.x === 0 &&
+//         map[block.position.y + 1]?.[block.position.x]?.type === 'teleporter'
+//       ) {
+//         style.borderBottomLeftRadius = 'none'
+//       }
 
-      if (
-        block.position.x === map[0].length - 1 &&
-        map[block.position.y + 1]?.[block.position.x]?.type === 'teleporter'
-      ) {
-        style.borderBottomRightRadius = 'none'
-      }
+//       if (
+//         block.position.x === map[0].length - 1 &&
+//         map[block.position.y + 1]?.[block.position.x]?.type === 'teleporter'
+//       ) {
+//         style.borderBottomRightRadius = 'none'
+//       }
 
-      if (
-        block.position.x === 0 &&
-        map[block.position.y - 1]?.[block.position.x]?.type === 'teleporter'
-      ) {
-        style.borderTopLeftRadius = 'none'
-      }
+//       if (
+//         block.position.x === 0 &&
+//         map[block.position.y - 1]?.[block.position.x]?.type === 'teleporter'
+//       ) {
+//         style.borderTopLeftRadius = 'none'
+//       }
 
-      if (
-        block.position.x === map[0].length - 1 &&
-        map[block.position.y - 1]?.[block.position.x]?.type === 'teleporter'
-      ) {
-        style.borderTopRightRadius = 'none'
-      }
-    }
+//       if (
+//         block.position.x === map[0].length - 1 &&
+//         map[block.position.y - 1]?.[block.position.x]?.type === 'teleporter'
+//       ) {
+//         style.borderTopRightRadius = 'none'
+//       }
+//     }
 
-    if (block.type === 'ghost-house') {
-      style.borderRadius = '0'
-      if (map[block.position.y][block.position.x + 1]?.type === 'ghost-house') {
-        style.borderRight = 'none'
-      }
+//     if (block.type === 'ghost-house') {
+//       style.borderRadius = '0'
+//       if (map[block.position.y][block.position.x + 1]?.type === 'ghost-house') {
+//         style.borderRight = 'none'
+//       }
 
-      if (map[block.position.y][block.position.x - 1]?.type === 'ghost-house') {
-        style.borderLeft = 'none'
-      }
+//       if (map[block.position.y][block.position.x - 1]?.type === 'ghost-house') {
+//         style.borderLeft = 'none'
+//       }
 
-      if (
-        map[block.position.y - 1]?.[block.position.x]?.type === 'ghost-house'
-      ) {
-        style.borderTop = 'none'
-      } else {
-        const middleX = Math.floor(map[0].length / 2)
-        const isMiddleDoor =
-          block.position.x === middleX || block.position.x === middleX - 1
+//       if (
+//         map[block.position.y - 1]?.[block.position.x]?.type === 'ghost-house'
+//       ) {
+//         style.borderTop = 'none'
+//       } else {
+//         const middleX = Math.floor(map[0].length / 2)
+//         const isMiddleDoor =
+//           block.position.x === middleX || block.position.x === middleX - 1
 
-        if (isMiddleDoor) {
-          style.borderTop = 'none'
-        }
-      }
+//         if (isMiddleDoor) {
+//           style.borderTop = 'none'
+//         }
+//       }
 
-      if (
-        map[block.position.y + 1]?.[block.position.x]?.type === 'ghost-house'
-      ) {
-        style.borderBottom = 'none'
-      }
-    }
+//       if (
+//         map[block.position.y + 1]?.[block.position.x]?.type === 'ghost-house'
+//       ) {
+//         style.borderBottom = 'none'
+//       }
+//     }
 
-    return style
-  }
+//     return style
+//   }
 
-  return (
-    <div className="map">
-      {map.map((row) =>
-        row.map((block) => {
-          return (
-            <div
-              onClick={() => {
-                console.log(block)
-              }}
-              key={`${block.position.x}-${block.position.y}`}
-              className={`block ${block.type}`}
-              style={getBlockStyle(block)}
-              title={`(${block.position.x}, ${block.position.y})`}
-            ></div>
-          )
-        }),
-      )}
-    </div>
-  )
-}
+//   return (
+//     <div className="map">
+//       {map.map((row) =>
+//         row.map((block) => {
+//           return (
+//             <div
+//               onClick={() => {
+//                 console.log(block)
+//               }}
+//               key={`${block.position.x}-${block.position.y}`}
+//               className={`block ${block.type}`}
+//               style={getBlockStyle(block)}
+//               title={`(${block.position.x}, ${block.position.y})`}
+//             ></div>
+//           )
+//         }),
+//       )}
+//     </div>
+//   )
+// }
